3cc2d409b990631165cb35e103ea19ed078edd68
Marcelo Damasceno de Melo <marcelo.damasceno@ifrn.edu.br>
2020-04-24 03:31:27 -0300

    fix #2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const twitter = require("./twitter/twitter-consumer");
 const express = require("express"),
-      app     = express(),
-      request = require("request");
+  app = express(),
+  request = require("request");
 
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
@@ -10,23 +11,36 @@ app.use(express.static("./node_modules/chart.js/"));
 app.use(express.static("./node_modules/popper.js/"));
 
 app.get("/", function (req, res) {
-    res.redirect("/inicio");
+  res.redirect("/inicio");
 });
 
-app.get("/inicio", function(req, res) {
-    request.get("https://brasil.io/api/dataset/covid19/caso/data?format=json&is_last=True&state=RN", function (err, resp, body) {
-        if (err) {
-            console.log("something went wrong :/");
-        } else {
-            res.render("pages/index", { data: JSON.parse(body) });
-        }
-    });
+app.get("/inicio", function (req, res) {
+  request.get(
+    "https://brasil.io/api/dataset/covid19/caso/data?format=json&is_last=True&state=RN",
+    function (err, resp, body) {
+      if (err) {
+        console.log("something went wrong :/");
+      } else {
+        res.render("pages/index", { data: JSON.parse(body) });
+      }
+    }
+  );
 });
 
-app.get("*", function(req,res) {
-    res.render("pages/notfound");
+app.get("/noticias", function (req, res) {
+  twitter.getTweets().then((tweets) => {
+    console.log(tweets);
+    res.render("pages/noticias", { data: tweets });
+  });
+
+  //
 });
 
-app.listen(process.env.PORT, function (){
-    console.log("Listening at " + process.env.PORT);
+app.get("*", function (req, res) {
+  res.render("pages/notfound");
+});
+
+process.env.PORT = 8080;
+app.listen(process.env.PORT, function () {
+  console.log("Listening at " + process.env.PORT || 8080);
 });
