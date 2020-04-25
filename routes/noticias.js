c22c136af9f6fd100dd027eb1aee02090a4d051f
Marcelo Damasceno de Melo <marcelo.damasceno@ifrn.edu.br>
2020-04-25 16:05:26 -0300

    refaoracao

--- routes/noticias.js
+++ routes/noticias.js
@@ -1,17 +1,15 @@
 const express = require("express"),
-      router  = express.Router({mergeParams: true}),
-      twitter = require("../twitter/twitter-consumer");
+  router = express.Router({ mergeParams: true }),
+  twitter = require("../twitter/twitter-consumer");
 
 router.get("/", (req, res) => {
   twitter.getTweets().then((tweets) => {
-    // console.log(tweets);
     res.render("pages/noticias", { data: tweets });
   });
   //
 });
 
 router.get("*", (req, res) => {
-  console.log("OOK");
   res.render("pages/notfound");
 });
 
