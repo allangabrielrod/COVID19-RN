4b337aefcea5126a14a2a4839f05ac8c9dd84654
Allan Gabriel <allan.gabriel@protonmail.com>
2020-04-25 21:44:16 -0300

    Ordena Tabela

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static("./public"));
 app.use(express.static("./node_modules/moment/min"));
 app.use(express.static("./node_modules/bootstrap/"));
+app.use(express.static("./node_modules/bootstrap-table/"));
 app.use(express.static("./node_modules/jquery/"));
 app.use(express.static("./node_modules/chart.js/"));
 app.use(express.static("./node_modules/popper.js/"));
--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,6 @@ router.get("/inicio", (req, res) => {
   );
 });
 
-//https://brasil.io/api/dataset/covid19/caso/data?state=RN
 router.get("/graficos", (req, res) => {
   request.get(
     "https://brasil.io/api/dataset/covid19/caso/data?format=json&is_last=True&state=RN",
